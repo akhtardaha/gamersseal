5f40bd3640a1f5a94c7906d3e4c883a0ad01d328
Umar <umar.arshad@g-it-s.com>
2015-05-07 12:00:38 +0500

    Signup page Api and Development

--- app/js/register.js
+++ app/js/register.js
@@ -1,107 +1,112 @@
 // JavaScript Document
 
-$(document).ready(function(){
-	console.log(window.localStorage.getItem("Gender"));
-	console.log(window.localStorage.getItem("SearchingFor"));
-	console.log(window.localStorage.getItem("Purpose"));
-	
-	$('#submit').click(function(){
+function registerUser(){
+		var fname = $('#fname').val();
+		var lname = $('#lname').val();
 		var username = $('#username').val();
 		var email = $('#email').val();
-		var land = $('#land').val();
-		var postcode = $('#postcode').val();
-		var zipcode = $('#zipcode').val();
-		var birthday = $('#birthday').val();
-		var gender = window.localStorage.getItem("Gender");	
-		var searchingFor = window.localStorage.getItem("SearchingFor");
-		var Purpose = window.localStorage.getItem("Purpose");
+		var australianId = $('#australianId').val();
+		var role = $('#userType').val();
 		
-		
-		if(username == '')
+		if(fname == '')
 		{
-			$('#username').css('background-color','#f6dbdb');
+			$('#fname').css('border','1px solid red');
 		}
 		else
 		{
-			$('#username').css('background-color','#fff');
+			$('#fname').css('border','1px solid #aaa');
+		}
+		if(lname == '')
+		{
+			$('#lname').css('border','1px solid red');
+		}
+		else
+		{
+			$('#lname').css('border','1px solid #aaa');
+		}
+		if(username == '')
+		{
+			$('#username').css('border','1px solid red');
+		}
+		else
+		{
+			$('#username').css('border','1px solid #aaa');
 		}
 		if(email == '')
 		{
-			$('#email').css('background-color','#f6dbdb');
+			$('#email').css('border','1px solid red');
 		}
 		else
 		{
-			$('#email').css('background-color','#fff');
+			$('#email').css('border','1px solid #aaa');
 		}
-		if(land == '')
+		if(australianId == '')
 		{
-			$('#land').css('background-color','#f6dbdb');
+			$('#australianId').css('border','1px solid red');
 		}
 		else
 		{
-			$('#land').css('background-color','#fff');
+			$('#australianId').css('border','1px solid #aaa');
 		}
-		if(postcode == '')
+		if(role == '')
 		{
-			$('#postcode').css('background-color','#f6dbdb');
+			$('.usertypeSelect .ui-btn').css('border','1px solid red');
 		}
 		else
 		{
-			$('#postcode').css('background-color','#fff');
+			$('.usertypeSelect .ui-btn').css('border','1px solid #aaa');
 		}
-		if(zipcode == '')
-		{
-			$('#zipcode').css('background-color','#f6dbdb');
-		}
-		else
-		{
-			$('#zipcode').css('background-color','#fff');
-		}
-		if(birthday == '')
-		{
-			$('#birthday').css('background-color','#f6dbdb');
-			$('label[for="birthday"]').css('background-color','#f6dbdb');
-		}
-		else
-		{
-			$('#birthday').css('background-color','#fff');
-			$('label[for="birthday"]').css('background-color','#fff');
-		}
-		if( username !='' && email != '' && land != '' && postcode != '' && zipcode != '' && birthday != '')
+		
+		if( username !='' && email != '' && fname != '' && lname != '' && australianId != '' && role != '')
 		{
 		if(validateEmail(email))
 		{
 			if(window.localStorage.getItem("emailExists") != 1 && window.localStorage.getItem("usernameExists") != 1)
 			{
 			$('#email').removeClass('error');
-			birthday = birthday+' 00:00:00';
-			$.ajax({
-				url:API_URL+'register/?key=1234567891011&username='+username+'&email='+email+'&display_name='+username,
-				type: "POST",
-				contentType: "application/json",
-				dataType: 'jsonp',
-				beforeSend: function (xhr){ 
-				xhr.setRequestHeader('Authorization', make_base_auth('bonjour', 'aj00pw')); 
-				},
-				success:function(data)
+			check = $("#terms").is(":checked");
+				if(check)
 				{
-					console.log(data);
-					if(data.status == 'ok')
+					if(role != 'Guest' || role != 'Standard' || role != 'Preminum')
 					{
-					var regCookie = data.cookie;
-					var regUserid = data.user_id;
-					extraProfileUpdate(regCookie,username,gender,searchingFor,Purpose,land,postcode,birthday);	
+						role = '';
 					}
-					else
-					{
-						console.log(data.status);
-					}
-					
-				},
-				error:function(){
-		
+					$.ajax({
+						url:API_URL+'register/?key=1234567891011&username='+username+'&email='+email+'&display_name='+username+'&first_name='+fname+'&last_name='+lname+'&role='+role,
+						type: "POST",
+						contentType: "application/json",
+						dataType: 'jsonp',
+						success:function(data)
+						{
+							console.log(data);
+							if(data.status == 'ok')
+							{
+							var regCookie = data.cookie;
+							var regUserid = data.user_id;
+							var fullname  = fname+' '+lname; 
+							extraProfileUpdate(regCookie,fullname,australianId);	
+							}
+							else
+							{
+								console.log(data.status);
+							}
+							
+						},
+						error:function(){
+				
+						}
+					});
 				}
-			});
+				else
+				{
+					console.log('Please accept Terms and Condition in order to Register');
+					navigator.notification.alert(
+						'Please accept Terms and Condition in order to Register',  // message
+						function(){},        // callback
+					   'Terms and Conditions',            // title
+						'OK'                  // buttonName
+					);	
+				}
 			}
 		}
 		else
@@ -116,19 +121,15 @@ $(document).ready(function(){
 		$('.fieldError').fadeIn();		
 	}
 		//console.log(birthday);
-	})
-})
+}
 
-function extraProfileUpdate(cookie,name,iam,searchingfor,purpose,land,city,birthday)
+function extraProfileUpdate(cookie,fullname,australianId)
 {
 	    $.ajax({
-        url:API_URL+'xprofile_update/?key=1234567891011&cookie='+cookie+'&Name='+name+'&Ich bin='+iam+'&Ich suche='+searchingfor+'&Rubrik='+purpose+'&Land='+land+'&City='+city+'&Birthday='+birthday+' ',
+        url:API_URL+'xprofile_update/?key=1234567891011&cookie='+cookie+'&Name='+fullname+'&Australian ID='+australianId+' ',
         type: "POST",
 		contentType: "application/json",
 		dataType: 'jsonp',
-		beforeSend: function (xhr){ 
-        xhr.setRequestHeader('Authorization', make_base_auth('bonjour', 'aj00pw')); 
-    	},
         success:function(data)
         {
 			console.log(data);
@@ -136,7 +137,7 @@ function extraProfileUpdate(cookie,name,iam,searchingfor,purpose,land,city,birth
 			{
 				navigator.notification.alert(
 						'Thankyou for your Registration. Please check your registration Email and Sigin to Continue',  // message
-						function(){setTimeout(function(){ window.location = 'signin.html'; }, 500);},        // callback
+						function(){setTimeout(function(){ window.location = 'login.html'; }, 200);},        // callback
 					   'Registration Completed',            // title
 						'OK'                  // buttonName
 					);	
