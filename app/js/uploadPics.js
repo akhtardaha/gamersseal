f755a044d2ac84b5cee68d9afdbf381a85422deb
Umar <umar.arshad@g-it-s.com>
2015-05-26 18:30:35 +0500

    Upload Js

---
+++ app/js/uploadPics.js
@@ -0,0 +1,62 @@
+ // Wait for PhoneGap to load
+        document.addEventListener("deviceready", onDeviceReady, false);
+        // PhoneGap is ready
+        function onDeviceReady() {
+ 		// Do cool things here...
+        }
+ 
+        function getImage() {
+            // Retrieve image file location from specified source
+            navigator.camera.getPicture(uploadPhoto, function(message) {
+				console.log('get picture failed');
+				},{
+				quality: 50, 
+				destinationType: navigator.camera.DestinationType.FILE_URI,
+				sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+			});
+ 
+        }
+ 
+        function uploadPhoto(imageURI) {
+			var cookie = window.localStorage.getItem("loginUserCookie");
+            var options = new FileUploadOptions();
+            options.fileKey="avatar";
+            options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+            options.mimeType="image/jpeg";
+			options.headers = {
+			  Connection: "close"
+		   };
+ 
+            var params = new Object();
+            params.key = "553789ba61dc1";
+			params.fName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+            params.cookie = cookie;
+ 
+            options.params = params;
+            options.chunkedMode = false;
+ 
+            var ft = new FileTransfer();
+			
+			//var url = http://bonjour.projekte-web.com/wp-content/themes/kleo/avatar_upload.php;
+			var url = 'http://gamersseal.com/development/upload.php';
+			console.log(url);
+			//upload_avatar(imageURI);
+            ft.upload(imageURI, url, win, fail, options, true);
+        }
+ 
+        function win(r) {
+            //console.log("Code = " + r.responseCode);
+            console.log("Response = " + r.response);
+			var PicName = r.response;
+			var oldPics = window.localStorage.getItem("GamesPics");
+			PicName = PicName+","+oldPics;
+			window.localStorage.setItem("GamesPics",PicName);
+            console.log(window.localStorage.getItem("GamesPics"));
+            //console.log(r);
+			//getUserInfo();
+        }
+ 
+        function fail(error) {
+            console.log("Error found, Error code ="+error.code);
+			//alert("An error has occurred: Code = " = error.code);
+        }
