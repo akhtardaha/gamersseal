2c6c477a6fee808a7a548627ffde6f90df1fb084
Umar <umar.arshad@g-it-s.com>
2015-06-12 13:08:30 +0500

    Cart Total Calculation on Cart page and Checkout Page

--- app/js/cart.js
+++ app/js/cart.js
@@ -40,6 +40,7 @@ function getProductsFromCart()
 							getPostPic(value.pid);
 						})
 						$('ul.products-list').html(html);
+						getCartTotal();
                     }
                     else
                     {
@@ -63,6 +64,52 @@ function getProductsFromCart()
 
 
 
+}
+function getCartTotal()
+{
+	var html = '';
+    db.transaction(
+        function(tx)
+        {
+            tx.executeSql('SELECT * FROM localcart', [],
+                function(tx,result)
+                {
+                   console.log(result.rows.length);
+                    if(result.rows.length>0)
+                    {
+                        var games = result.rows;
+						var totalCart = 0;
+						 $.each(games, function(key,value){
+							var pPrice = value.price;
+							totalCart = parseFloat(totalCart)+parseFloat(pPrice); 
+						})
+						var gstPercenet = 9;
+						var gst = Math.round(totalCart/100 * gstPercenet);
+						var shippingCost = 20;
+						var OrderTotal = parseFloat(totalCart) + parseFloat(gst) + parseFloat(shippingCost);
+						html += '<div class="CartTotal">Cart Total = '+totalCart.toFixed(2)+'$</div>';
+						html += '<div class="CartGst">GST '+gstPercenet+'% = '+gst.toFixed(2)+'$</div>';
+						html += '<div class="CartShippingcost">Shipping Cost = '+shippingCost.toFixed(2)+'$</div>';
+						
+						html += '<div class="CartTotal">Order Total = '+OrderTotal.toFixed(2)+'$</div>';
+						
+						$('.calculation').html(html);
+                	}
+				}
+                ,function(err){
+                    console.log('There is some error while getting Total from Cart');
+
+                });
+
+        },
+        function(err){
+            console.log('Error: '+err.message);
+             console.log('Error: There is some error while getting Total Cart please try again.');
+        },
+        function(){
+            console.log('Success: Cart Total fetch successfully');
+        }
+        );
 }
 
 function getPostPic(post_id)
