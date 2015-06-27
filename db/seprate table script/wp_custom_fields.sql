bf2e15438f9395e2a1e1b89ed940524b3a395b73
Ch Fiaz <ch.fiazmcs@gmail.com>
2015-06-27 23:56:25 +0500

    Script for to create custom fields table

---
+++ db/seprate table script/wp_custom_fields.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.6
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 27, 2015 at 08:53 PM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `development`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_custom_fields`
+--
+
+CREATE TABLE IF NOT EXISTS `wp_custom_fields` (
+  `post_id` int(11) NOT NULL,
+  `age_limit` varchar(5) DEFAULT NULL,
+  `product_price` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
