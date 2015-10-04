f2fa0cbf03d687605aea9a34a8b44871bc4e0180
chfiaz <ch.fiazmcs@gmail.com>
2015-10-04 22:43:50 +0500

    add custom event post

--- wp-content/themes/twentyfifteen/functions-custom.php
+++ wp-content/themes/twentyfifteen/functions-custom.php
@@ -701,5 +701,142 @@ function save_home_settings() {
     );
 }
 
+add_action( 'init', 'create_posttype_event' );
+function create_posttype_event() {
+    register_post_type( 'g_event',
+		array(
+			'labels' => array(
+				'name' => __( 'Events' ),
+				'singular_name' => __( 'Event' ),
+				'add_new' => __( 'Add New Event' ),
+				'add_new_item' => __( 'Add New Event' ),
+				'edit_item' => __( 'Edit Event' ),
+				'new_item' => __( 'Add New Event' ),
+				'view_item' => __( 'View Event' ),
+				'search_items' => __( 'Search Event' ),
+				'not_found' => __( 'No events found' ),
+				'not_found_in_trash' => __( 'No events found in trash' )
+			),
+			'public' => true,
+			'supports' => array( 'title', 'editor', 'thumbnail', 'comments' ),
+			'capability_type' => 'post',
+			'rewrite' => array("slug" => "events"), // Permalinks format
+			'menu_position' => 5,
+			'register_meta_box_cb' => 'add_events_metaboxes'
+		)
+	);
+  register_taxonomy_for_object_type( 'category', 'g_event' );
+}
+
+// Add the Events Meta Boxes
+
+function add_events_metaboxes() {
+	add_meta_box('wpt_events_date', 'Event Date', 'wpt_events_date', 'g_event', 'side', 'low');
+	add_meta_box('wpt_events_location', 'Event Location', 'wpt_events_location', 'g_event', 'normal', 'high');
+        add_meta_box('wpt_events_cost', 'Event Cost', 'wpt_events_cost', 'g_event', 'side', 'low');
+        add_meta_box('wpt_events_requirement', 'Requirements for attendance ', 'wpt_events_requirement', 'g_event', 'normal', 'high');
+}
+
+// The Event Location Metabox
+
+function wpt_events_location() {
+	global $post;
+	
+	// Noncename needed to verify where the data originated
+	echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' . 
+	wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+	
+	// Get the location data if its already been entered
+	$location = get_post_meta($post->ID, '_location', true);
+	
+	// Echo out the field
+	echo '<input type="text" name="_location" value="' . $location  . '" class="widefat" />';
+
+}
+
+function wpt_events_cost() {
+	global $post;
+	
+	// Noncename needed to verify where the data originated
+	echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' . 
+	wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+	
+	// Get the location data if its already been entered
+	$cost = get_post_meta($post->ID, '_cost', true);
+	
+	// Echo out the field
+	echo '<input type="text" name="_cost" value="' . $cost  . '" class="widefat" />';
+
+}
+
+function wpt_events_requirement() {
+	global $post;
+	
+	// Noncename needed to verify where the data originated
+	echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' . 
+	wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+	
+	// Get the location data if its already been entered
+	$requirement = get_post_meta($post->ID, '_requirement', true);
+	
+	// Echo out the field
+	echo '<input type="text" name="_requirement" value="' . $requirement  . '" class="widefat" />';
+
+}
+function wpt_events_date() {
+	global $post;
+	
+	// Noncename needed to verify where the data originated
+	echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' . 
+	wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+	
+	// Get the location data if its already been entered
+	$date = get_post_meta($post->ID, '_date', true);
+	
+	// Echo out the field
+	echo '<input type="text" name="_date" value="' . $date  . '" class="widefat" />';
+
+}
+
+// Save the Metabox Data
+
+function wpt_save_events_meta($post_id, $post) {
+	
+	// verify this came from the our screen and with proper authorization,
+	// because save_post can be triggered at other times
+	if ( !wp_verify_nonce( $_POST['eventmeta_noncename'], plugin_basename(__FILE__) )) {
+	return $post->ID;
+	}
+
+	// Is the user allowed to edit the post or page?
+	if ( !current_user_can( 'edit_post', $post->ID ))
+		return $post->ID;
+
+	// OK, we're authenticated: we need to find and save the data
+	// We'll put it into an array to make it easier to loop though.
+	
+	$events_meta['_location'] = $_POST['_location'];
+        $events_meta['_date'] = $_POST['_date'];
+        $events_meta['_cost'] = $_POST['_cost'];
+        $events_meta['_requirement'] = $_POST['_requirement'];
+	
+	// Add values of $events_meta as custom fields
+	
+	foreach ($events_meta as $key => $value) { // Cycle through the $events_meta array!
+		if( $post->post_type == 'revision' ) return; // Don't store custom data twice
+		$value = implode(',', (array)$value); // If $value is an array, make it a CSV (unlikely)
+		if(get_post_meta($post->ID, $key, FALSE)) { // If the custom field already has a value
+			update_post_meta($post->ID, $key, $value);
+		} else { // If the custom field doesn't have a value
+			add_post_meta($post->ID, $key, $value);
+		}
+		if(!$value) delete_post_meta($post->ID, $key); // Delete if blank
+	}
+
+}
+
+add_action('save_post', 'wpt_save_events_meta', 1, 2); // save the custom fields
+
+
 
 
